515f4aee2755cf16ec1a26c24a0b4f5616f5a7f2
Arnav Sood (VSE Syzygy) <me@arnavsood.com>
2019-09-17 22:11:42 +0000

    order of operations with containers

--- reset-servers.sh
+++ reset-servers.sh
@@ -1,11 +1,12 @@
 # Stop/shut down all containers
 sudo docker stop $(sudo docker ps --format {{.Names}} | grep jupyter)
 
+# Remove containers 
+sudo docker rm $(sudo docker ps -aq)
+
 # Remove .julia directories 
 for d in /tank/home/*; do
   echo "cleaing $d"
   sudo rm -rf d/.julia
 done
 
-# Remove containers 
-sudo docker rm $(sudo docker ps -aq)
